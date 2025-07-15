eee4a5a64173e6741cbffec5583f3dce4af7aad0
mkzopio <mk@zopio.com>
2025-07-15 04:41:30 +0300

    feat: add monorepo configuration with turbo generator and build setup

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+/**
+ * SPDX-License-Identifier: MIT
+ */
+
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['scripts/index.ts'],
+  outDir: 'dist',
+  sourcemap: false,
+  minify: true,
+  dts: true,
+  format: ['cjs', 'esm'],
+});
